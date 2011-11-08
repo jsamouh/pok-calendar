62ef6ede23119f7a6defa0ad43035dff3af1e3d7
Jordan Samouh <lifeextension25@gmail.com>
2011-11-08 15:29:48 +0100

    1st commit

---
+++ app/cache/dev/twig/34/b0/da56c171c2ff1c0b198132a4b7eb.php
@@ -0,0 +1,23 @@
+<?php
+
+/* AcmeDemoBundle:Welcome:index.html.twig */
+class __TwigTemplate_34b0da56c171c2ff1c0b198132a4b7eb extends Twig_Template
+{
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $context = array_merge($this->env->getGlobals(), $context);
+
+        // line 1
+        echo "OK";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeDemoBundle:Welcome:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
